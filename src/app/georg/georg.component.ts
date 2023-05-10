5c6c237f6dcba87e513abad5ec3e0309af877b45
Georg <georg.strassberger@googlemail.com>
2023-05-10 20:41:54 +0200

    created new component GEORG

---
+++ src/app/georg/georg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-georg',
+  templateUrl: './georg.component.html',
+  styleUrls: ['./georg.component.scss']
+})
+export class GeorgComponent {
+
+}
