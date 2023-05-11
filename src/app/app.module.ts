69b26edac67685c412bbfb12fb1b7ed885a81cc1
Georg <georg.strassberger@googlemail.com>
2023-05-11 16:47:40 +0200

    created channels/directmsg components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 import { WelcomeComponent } from './page/welcome/welcome.component';
 import { MainComponent } from './page/main/main.component';
+import { DirectMessagesComponent } from './components/direct-messages/direct-messages.component';
+import { ChannelsComponent } from './components/channels/channels.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { MainComponent } from './page/main/main.component';
     PageNotFoundComponent,
     WelcomeComponent,
     MainComponent,
+    DirectMessagesComponent,
+    ChannelsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
