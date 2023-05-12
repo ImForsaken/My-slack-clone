4dbf52d9c8086b84bc8c8803132b8540618ddc92
Kevin Herbst <kevin-herbst1993@web.de>
2023-05-12 03:32:20 +0200

    added auth comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page/page-not-found/page-not-found.component';
 import { WelcomeComponent } from './page/welcome/welcome.component';
 import { MainComponent } from './page/main/main.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { MainComponent } from './page/main/main.component';
     PageNotFoundComponent,
     WelcomeComponent,
     MainComponent,
+    AuthComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
