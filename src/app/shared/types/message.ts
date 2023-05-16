6869867fbfde243ac41a4ef3f8bee71dd63ecbec
Dennis Ammen <dennis.ammen@outlook.com>
2023-05-16 15:45:02 +0200

    Added types

---
+++ src/app/shared/types/message.ts
@@ -0,0 +1,6 @@
+export type Message = {
+    userId: string,
+    userName: string,
+    text: string,
+    timestamp: string
+}
---
+++ src/app/shared/types/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+    id?: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    profilePicture: string,
+    chats: string[]
+}
