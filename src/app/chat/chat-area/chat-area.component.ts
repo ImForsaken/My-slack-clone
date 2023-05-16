4bb2312f90790d45005cb14255f571b08959cbd0
Dennis Ammen <dennis.ammen@outlook.com>
2023-05-16 15:50:04 +0200

    Modified chat

--- src/app/chat/chat-area/chat-area.component.ts
+++ src/app/chat/chat-area/chat-area.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ChatDbService } from 'src/app/shared/services/chat-db.service';
+import { UserDbService } from 'src/app/shared/services/user-db.service';
+import { Message } from 'src/app/shared/types/message';
+import { User } from 'src/app/shared/types/user';
 
 @Component({
   selector: 'app-chat-area',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-area.component.scss']
 })
 export class ChatAreaComponent {
+  chatService: ChatDbService = inject(ChatDbService);
+  userService: UserDbService = inject(UserDbService);
 
+  chatId: string = 'bHADuOvmaLFl970vTDFK'; // Wird bei Click auf Chat an Chat Komponente übergenen.
+
+  constructor() {
+    const user: User = {
+      firstname: 'Maria',
+      lastname: 'Tester',
+      email: 'tester@mail.com',
+      profilePicture: 'imageUrl',
+      chats: [
+        'bHADuOvmaLFl970vTDFK'
+      ]
+    }
+    console.log(this.userService.createUser(user));
+  }
+
+  // messages: Message[] = [
+  //   {
+  //     userId: 'Dennis Ammen',
+  //     text: 'Hallo leude!',
+  //     timestamp: '16:13'
+  //   },
+  //   {
+  //     userId: 'Dennis Ammen',
+  //     text: '<p>sdfl kjsdf sldkjf sdlkfj lsdfj sldkfj sdlf jweop perot epro terpto erop ioper eoprt ieoprt ieprot ipert igpdfg ölekrtöd fgokedrö odiopgö kdörlk gdoöfgk örgdfo gkdrö gkdfög klrdöodfg kdrölkg födokr öldfkg ödrgk ödflkg </p>',
+  //     timestamp: '15:11'
+  //   },
+  //   {
+  //     userId: 'Dennis Ammen',
+  //     text: '<p>sdfsldfkjsdflsdkfjsdlfksjdflsdkfjsdlkfjsdflksfjsdlfkjsdfklsfjslkfjsdklfjsdklfsjdflksdjfkl sdlkfjs lsdkfj skldfjkljsdflksjdflksdjf sldkfj klsdfj skldfjklsdfj lskdfj lsdkfj slkdfj lksjdfl sjdf lksdjf lksdfj lksdjf sdf</p>',
+  //     timestamp: '17:09'
+  //   },
+  //   {
+  //     userId: 'Dennis Ammen',
+  //     text: `<p>sdfsfsf</p><pre class="ql-syntax" spellcheck="false">sdfsf
+  //     </pre><p>sfsffasdad</p><p><br></p><pre class="ql-syntax" spellcheck="false">asdasdasd
+      
+  //     asdasd
+  //     </pre><p><br></p>`,
+  //     timestamp: '11:57'
+  //   },
+  // ]
 }
--- src/app/chat/text-editor/text-editor.component.ts
+++ src/app/chat/text-editor/text-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chat } from 'src/app/shared/types/chat';
 
 @Component({
   selector: 'app-text-editor',
@@ -8,9 +9,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class TextEditorComponent {
-  editor = {
+  text!: string;
+  quillStyle = {
     border: '2px solid #cac0cb',
     borderTop: 'none',
     borderRadius: '0 0 10px 10px'
   }
+
+  chat: Chat = {
+    name: 'Test',
+    createdOn: ''
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      // console.log(this.text);
+      this.text = '';
+    }
+  }
+
 }
