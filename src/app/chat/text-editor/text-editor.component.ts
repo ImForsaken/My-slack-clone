3726d25fa1ccfe254f718321b601e34328948509
Dennis Ammen <dennis.ammen@outlook.com>
2023-05-17 19:51:24 +0200

    Save messages in db on enter

--- src/app/chat/text-editor/text-editor.component.ts
+++ src/app/chat/text-editor/text-editor.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { ChatDbService } from 'src/app/shared/service/chat-db.service';
 import { Chat } from 'src/app/shared/types/chat';
+import { Message } from 'src/app/shared/types/message';
 
 @Component({
   selector: 'app-text-editor',
@@ -9,21 +11,28 @@ import { Chat } from 'src/app/shared/types/chat';
   ]
 })
 export class TextEditorComponent {
+  @Input() chatId!: string;
+  chatService: ChatDbService = inject(ChatDbService);
   text!: string;
+
   quillStyle = {
     border: '2px solid #cac0cb',
     borderTop: 'none',
     borderRadius: '0 0 10px 10px'
   }
 
-  chat: Chat = {
-    name: 'Test',
-    createdOn: ''
-  }
-
   onKeyDown(event: KeyboardEvent) {
     if (event.key === 'Enter') {
-      // console.log(this.text);
+      const date = new Date();
+      const message: Message = {
+        userId: 'xyz',
+        userName: 'Dennis Ammen',
+        text: this.text,
+        timestamp: date.toISOString()
+      }
+
+      this.chatService.addMessage(this.chatId, message);
+
       this.text = '';
     }
   }
