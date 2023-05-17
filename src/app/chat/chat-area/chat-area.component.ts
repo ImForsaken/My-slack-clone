297b61ffb7ea1348b1f853aa9ce33f61e545763e
Dennis Ammen <dennis.ammen@outlook.com>
2023-05-17 19:49:48 +0200

    Render db messages

--- src/app/chat/chat-area/chat-area.component.ts
+++ src/app/chat/chat-area/chat-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { ChatDbService } from 'src/app/shared/service/chat-db.service';
 import { UserDbService } from 'src/app/shared/service/user-db.service';
 import { Message } from 'src/app/shared/types/message';
@@ -12,46 +13,10 @@ import { User } from 'src/app/shared/types/user';
 export class ChatAreaComponent {
   chatService: ChatDbService = inject(ChatDbService);
   userService: UserDbService = inject(UserDbService);
-
   chatId: string = 'bHADuOvmaLFl970vTDFK'; // Wird bei Click auf Chat an Chat Komponente übergeben.
+  messages!: Observable<Message[]>;
 
   constructor() {
-    const user: User = {
-      firstname: 'Maria',
-      lastname: 'Tester',
-      email: 'tester@mail.com',
-      profilePicture: 'imageUrl',
-      chats: [
-        'bHADuOvmaLFl970vTDFK'
-      ]
-    }
-    console.log(this.userService.createUser(user));
+    this.messages = this.chatService.getMessages$(this.chatId)
   }
-
-  // messages: Message[] = [
-  //   {
-  //     userId: 'Dennis Ammen',
-  //     text: 'Hallo leude!',
-  //     timestamp: '16:13'
-  //   },
-  //   {
-  //     userId: 'Dennis Ammen',
-  //     text: '<p>sdfl kjsdf sldkjf sdlkfj lsdfj sldkfj sdlf jweop perot epro terpto erop ioper eoprt ieoprt ieprot ipert igpdfg ölekrtöd fgokedrö odiopgö kdörlk gdoöfgk örgdfo gkdrö gkdfög klrdöodfg kdrölkg födokr öldfkg ödrgk ödflkg </p>',
-  //     timestamp: '15:11'
-  //   },
-  //   {
-  //     userId: 'Dennis Ammen',
-  //     text: '<p>sdfsldfkjsdflsdkfjsdlfksjdflsdkfjsdlkfjsdflksfjsdlfkjsdfklsfjslkfjsdklfjsdklfsjdflksdjfkl sdlkfjs lsdkfj skldfjkljsdflksjdflksdjf sldkfj klsdfj skldfjklsdfj lskdfj lsdkfj slkdfj lksjdfl sjdf lksdjf lksdfj lksdjf sdf</p>',
-  //     timestamp: '17:09'
-  //   },
-  //   {
-  //     userId: 'Dennis Ammen',
-  //     text: `<p>sdfsfsf</p><pre class="ql-syntax" spellcheck="false">sdfsf
-  //     </pre><p>sfsffasdad</p><p><br></p><pre class="ql-syntax" spellcheck="false">asdasdasd
-      
-  //     asdasd
-  //     </pre><p><br></p>`,
-  //     timestamp: '11:57'
-  //   },
-  // ]
 }
