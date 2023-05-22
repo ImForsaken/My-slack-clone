39630d6556e8d6bd2fd9e95d64e4741a2736ab98
Kevin Herbst <kevin-herbst1993@web.de>
2023-05-22 17:11:23 +0200

    test

--- src/app/shared/types/user.ts
+++ src/app/shared/types/user.ts
@@ -1,12 +1,12 @@
-import { Chat } from "./chat"
+import { Chat } from './chat';
 
-export type User = {
-    id?: string,
-    firstname: string,
-    lastname: string,
-    email: string,
-    profilePicture: string,
-    channels: Chat[],
-    directMessages: Chat[],
-    isOnline: boolean
-}
+export type TUser = {
+  id?: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  profilePicture: string;
+  channels: Chat[];
+  directMessages: Chat[];
+  isOnline: boolean;
+};
