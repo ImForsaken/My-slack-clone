44ec8076a3a0c9e8ec95b33f19024e14da7bbb7d
Georg <georg.strassberger@googlemail.com>
2023-06-13 16:41:58 +0200

    adding dynamic username to chat-hint

--- src/app/components/info/chat-hint/chat-hint.component.ts
+++ src/app/components/info/chat-hint/chat-hint.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/shared/service/store.service';
+import { TUser } from 'src/app/shared/types/user';
 
 @Component({
   selector: 'app-chat-hint',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-hint.component.scss'],
 })
 export class ChatHintComponent {
-  username: string = 'Username';
+  userSub$!: Subscription;
+  user: TUser | null = null;
+
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.userSub$ = this.storeService.currentUser$.subscribe((user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSub$) {
+      this.userSub$.unsubscribe();
+    }
+  }
 }
