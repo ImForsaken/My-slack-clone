a0835e8d5dfcccb8a78b9aff2a8289a74604a4d5
Dennis Ammen <dennis.ammen@outlook.com>
2023-06-28 21:52:43 +0200

    Added setSidenavMode and onWindowResize

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -4,7 +4,7 @@ import {
   OnDestroy,
   OnInit,
   ViewChild,
-  inject,
+  HostListener
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
@@ -30,6 +30,11 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
   activeChannel: TChannel | null = null;
   activeUserChat: TUser | null = null;
 
+  private resizeThreshold = 600;
+  private hasReachedThreshold = false;
+  private previousWidth = window.innerWidth;
+
+
   constructor(
     public dialog: MatDialog,
     public userService: UserDbService,
@@ -42,6 +47,7 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
    */
   ngOnInit(): void {
     this.getActiveChat();
+    this.setSidenavMode();
   }
 
   ngAfterViewInit(): void {
@@ -78,6 +84,38 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
     } else {
       this.modeValue = 'side';
     }
+  }
+
+  /**
+   * Sets the sidenav mode on component initialisation.
+   */
+  setSidenavMode() {
+    const currentWidth = window.innerWidth;
+    if (currentWidth > this.resizeThreshold) {
+      this.modeValue = 'side';
+    } else {
+      this.modeValue = 'over';
+    }
+  }
+
+  /**
+   * Opens or closes the sidenav once the innerWidth threshold is exceeded/underrun.
+   */
+  @HostListener('window:resize')
+  onWindowResize() {
+    const currentWidth = window.innerWidth;
+    const thresholdExceeded = this.previousWidth <= this.resizeThreshold && currentWidth > this.resizeThreshold;
+    const thresholdUnderrun = this.previousWidth > this.resizeThreshold && currentWidth <= this.resizeThreshold;
+
+    if (thresholdExceeded && !this.hasReachedThreshold) {
+      this.hasReachedThreshold = true;
+      this.isSidenavOpened = true;
+    } else if (thresholdUnderrun && this.hasReachedThreshold) {
+      this.hasReachedThreshold = false;
+      this.isSidenavOpened = false;
+    }
+
+    this.previousWidth = currentWidth;
   }
 
   /**
